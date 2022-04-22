3400046b1717241599e10a95afc4594080781e32
Ryzzan Abbade Salman <ryzzan@pop-os.localdomain>
2022-04-22 13:55:06 -0300

    Fix: remove-confirmation-dialog

--- src/app/components/remove-confirmation-dialog/remove-confirmation-dialog.component.ts
+++ src/app/components/remove-confirmation-dialog/remove-confirmation-dialog.component.ts
@@ -1,58 +1,22 @@
+import { Component, Inject } from "@angular/core";
 import {
-  Component, Inject
-} from '@angular/core';
-import {
-  FormArray,
-  FormBuilder,
-  FormGroup,
-  Validators
-} from '@angular/forms';
-import {
-  ActivatedRoute,
-  Router
-} from '@angular/router';
-import {
-  MatDialog,
   MatDialogRef,
-  MAT_DIALOG_DATA
-} from '@angular/material/dialog';
-export interface RemoveConfirmationDialogDialogData {
-    teste: string,
-    teste2: string,
-}
+  MAT_DIALOG_DATA,
+} from "@angular/material/dialog";
+import { DialogData } from "../logout-confirmation-dialog/logout-confirmation-dialog.component";
+
 @Component({
-  selector: 'app-remove-confirmation-dialog',
-  templateUrl: './remove-confirmation-dialog.component.html',
-  styleUrls: ['./remove-confirmation-dialog.component.scss']
-}) export class RemoveConfirmationDialogComponent {
-  id: string = this.removeConfirmationDialogRoute.snapshot.params['id'];
-  isAddModule: boolean = !this.id;
+  selector: "app-remove-confirmation-dialog",
+  templateUrl: "./remove-confirmation-dialog.component.html",
+  styleUrls: ["./remove-confirmation-dialog.component.scss"],
+})
+export class RemoveConfirmationDialogComponent {
   constructor(
-    private removeConfirmationDialogFormBuilder: FormBuilder, 
-    private removeConfirmationDialogRoute: ActivatedRoute, 
-    private removeConfirmationDialogRouter: Router, 
-    public removeConfirmationDialogDialogRef: MatDialogRef<RemoveConfirmationDialogComponent>, 
-    @Inject(MAT_DIALOG_DATA) public data: RemoveConfirmationDialogDialogData) {
-    this.setForm();
-  };
-  removeConfirmationDialogForm = this.removeConfirmationDialogFormBuilder.group({});
-  setForm = () => {
-    if (!this.isAddModule) {
-      /** TO-DO */ /** Service this.userService.getById(this.id).pipe(first()).subscribe(x => this.form.patchValue(x));*/
-    }
-  }
-  removeConfirmationDialogSubmit = () => {
-    fetch('$ENV$/acl', {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-      },
-      body: JSON.stringify(this.removeConfirmationDialogForm.value),
-    }).then((data) => {
-      data.json().then((keys) => {})
-    });
-  }
+    public removeConfirmationDialogDialogRef: MatDialogRef<
+      RemoveConfirmationDialogComponent
+    >,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
   RemoveConfirmationDialogOnNoClick(): void {
     this.removeConfirmationDialogDialogRef.close();
   }
