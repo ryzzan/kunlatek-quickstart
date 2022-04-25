b69d3c6da30caeb2d14061446523cec245f82388
Ryzzan Abbade Salman <ryzzan@pop-os.localdomain>
2022-04-25 13:11:56 -0300

    Replace locate replace by redirectTo

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,21 +1,22 @@
-import {
-  Component
-} from '@angular/core';
-import { AuthService } from './auth.service';
+import { Component } from "@angular/core";
+import { AuthService } from "./auth.service";
 
-import { MyErrorHandler } from '../../utils/error-handler';
-import { ActivatedRoute, Router } from '@angular/router';
+import { MyErrorHandler } from "../../utils/error-handler";
+import { ActivatedRoute, Router } from "@angular/router";
 
-import { UserInterface } from '../../interfaces/autentikigo';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { environment } from 'src/environments/environment';
+import { UserInterface } from "../../interfaces/autentikigo";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { environment } from "src/environments/environment";
+
+export interface ParamsI {
+  token?: string;
+}
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
-
 export class LoginComponent {
   user?: any;
   loggedIn = false;
@@ -27,80 +28,91 @@ export class LoginComponent {
     private _auth: AuthService,
     private _errorHandler: MyErrorHandler,
     private _snackbar: MatSnackBar
-  ) {}
-
-  ngOnInit(): void {
-    this._auth.signOut();
-
-    const params = this.route.snapshot.queryParams;
-    if (params['token']) {
-      const token = params['token'];
-      
-      this._auth.getUserData(token)
-      .then((res: any) => {
-        if (res.statusCode === 200) {
-          this.setSessionStorage(res.data);
-          this.router.navigate(['/main']);
-        }
-
-        if (res.statusCode === 601) {
-          sessionStorage.setItem('tokenToRegister', token);
-          this.router.navigate(['/signup']);
-        }
-      })
-      .catch(err => {
-        if (err.message) {
-          const message = this._errorHandler.apiErrorMessage(err.message);
-          this.sendErrorMessage(message);
-        }
-      });
-    }
-
-    if (sessionStorage.getItem('_id')) {
-      this.router.navigate(['/main']);      
-    }
+  ) {
+    this.setAuthentication();
   }
 
   signInWithGoogle = () => {
-    window.location.replace(`${environment.baseUrl}/auth/google-signin`);
-  }
+    this.redirectTo("auth/google-signin");
+  };
 
   signInWithApple = () => {
-    window.location.replace(`${environment.baseUrl}/auth/apple-signin`);
-  }
+    this.redirectTo("auth/apple-signin");
+  };
+
+  setAuthentication = async () => {
+    this._auth.signOut();
+    const params: ParamsI = this.route.snapshot.queryParams;
+    
+    if (params.token) {
+      const token = params.token;
+      try {        
+        const result: any = await this._auth
+                              .getUserData(token);
+        if (result?.statusCode === 200) {
+          this.setSessionStorage(result.data);
+          this.router.navigate(["/main"]);
+        }
+
+        if (result?.statusCode === 601) {
+          sessionStorage.setItem("tokenToRegister", token);
+          this.router.navigate(["/signup"]);
+        }
+      } catch (error: any) {
+        const message = this._errorHandler.apiErrorMessage(error?.message);
+        this.sendErrorMessage(message);
+      }
+    }
+
+    if (sessionStorage.getItem("_id")) {
+      this.router.navigate(["/main"]);
+    }
+  };
 
   setSessionStorage = (data: UserInterface) => {
-    sessionStorage.setItem('_id', data.userData._id);
-    sessionStorage.setItem('token', data.authToken);
-    sessionStorage.setItem('refreshToken', data.authRefreshToken);
-    sessionStorage.setItem('email', data.userData.email);
-    sessionStorage.setItem('permission', JSON.stringify(data.userData.permissionGroups));
+    sessionStorage.setItem("_id", data.userData._id);
+    sessionStorage.setItem("token", data.authToken);
+    sessionStorage.setItem("refreshToken", data.authRefreshToken);
+    sessionStorage.setItem("email", data.userData.email);
+    sessionStorage.setItem(
+      "permission",
+      JSON.stringify(data.userData.permissionGroups)
+    );
 
     if (data.userData.person) {
-      sessionStorage.setItem('birthday', data.userData.person.birthday);
-      sessionStorage.setItem('country', data.userData.person.country);
-      sessionStorage.setItem('gender', data.userData.person.gender);
-      sessionStorage.setItem('mother', data.userData.person.mother);
-      sessionStorage.setItem('name', data.userData.person.name);
-      sessionStorage.setItem('uniqueId', data.userData.person.uniqueId);
-      sessionStorage.setItem('personId', data.userData.person._id);
+      sessionStorage.setItem("birthday", data.userData.person.birthday);
+      sessionStorage.setItem("country", data.userData.person.country);
+      sessionStorage.setItem("gender", data.userData.person.gender);
+      sessionStorage.setItem("mother", data.userData.person.mother);
+      sessionStorage.setItem("name", data.userData.person.name);
+      sessionStorage.setItem("uniqueId", data.userData.person.uniqueId);
+      sessionStorage.setItem("personId", data.userData.person._id);
     }
 
     if (data.userData.company) {
-      sessionStorage.setItem('birthday', data.userData.company.birthday);
-      sessionStorage.setItem('cnae', data.userData.company.cnae);
-      sessionStorage.setItem('corporateName', data.userData.company.corporateName);
-      sessionStorage.setItem('tradeName', data.userData.company.tradeName);
-      sessionStorage.setItem('companyEmail', data.userData.company.email);
-      sessionStorage.setItem('responsible', data.userData.company.responsible);
-      sessionStorage.setItem('uniqueId', data.userData.company.uniqueId);
-      sessionStorage.setItem('companyId', data.userData.company._id);
+      sessionStorage.setItem("birthday", data.userData.company.birthday);
+      sessionStorage.setItem("cnae", data.userData.company.cnae);
+      sessionStorage.setItem(
+        "corporateName",
+        data.userData.company.corporateName
+      );
+      sessionStorage.setItem("tradeName", data.userData.company.tradeName);
+      sessionStorage.setItem("companyEmail", data.userData.company.email);
+      sessionStorage.setItem("responsible", data.userData.company.responsible);
+      sessionStorage.setItem("uniqueId", data.userData.company.uniqueId);
+      sessionStorage.setItem("companyId", data.userData.company._id);
     }
-  }
+  };
 
   sendErrorMessage = (errorMessage: string) => {
     this._snackbar.open(errorMessage, undefined, {
-        duration: 4 * 1000,
+      duration: 4 * 1000,
     });
-  }
+  };
+
+  redirectTo(uri: string) {
+    this.router.navigateByUrl("/", { skipLocationChange: true }).then(() => {
+      this.router.navigate([uri]);
+    });
+  };
 }
